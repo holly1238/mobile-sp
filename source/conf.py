2f707725036a1903ab31aa2c50ae13e7fd0cdd87
Brian Johnson <brianjo@fb.com>
2021-08-12 14:35:41 -0400

    testing theme options

--- source/conf.py
+++ source/conf.py
@@ -57,6 +57,15 @@ exclude_patterns = []
 html_theme = 'pytorch_sphinx_theme'
 html_theme_path = [pytorch_sphinx_theme.get_html_theme_path()]
 
+html_theme_options = {
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
